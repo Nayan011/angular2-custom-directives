9e1b8f15086a47a8a75bc6ece418cdc420ac9226
Nuruzzaman Bepari <30918905+Nayan011@users.noreply.github.com>
2017-09-20 14:38:59 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h3>Changed color is {{value}}<h3>
+  <h3>Changed color from child component is {{value}}<h3>
    <child-app (colorChange)="getValue($event)"><child-app>
   `,
   styleUrls: ['./app.component.css']
