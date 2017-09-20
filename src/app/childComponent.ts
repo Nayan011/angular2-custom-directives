b67515d8e2441fc71b3a26ce3f988b534bc9ae74
Nuruzzaman Bepari <30918905+Nayan011@users.noreply.github.com>
2017-09-20 14:39:53 -0500

    Update childComponent.ts

--- src/app/childComponent.ts
+++ src/app/childComponent.ts
@@ -1,30 +1,33 @@
-import { Component,Output,OnInit,EventEmitter,HostBinding,HostListener,ElementRef, Renderer } from '@angular/core';
+import { Component,Output,OnInit,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'child-app',
   template: `
    <div>
-   <p>mouseenter to show uppercase and click to hide or show</p>
-    <ul  visibility>
-      <li clas="cl" *ngFor="let name of names" upper1 mycolor >{{name}}</li><br>
+   
+   <p>mouseenter to show uppercase, double click to hide or show and click just to send color to parent</p>
+    <ul  >
+      <li class="cl" *ngFor="let name of names" upper1 mycolor visibility (click)="getColor(myli)" #myli >{{name}}</li><br>
     </ul>
    </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class childComponent implements OnInit {
-constructor(private e: ElementRef,
-              private r: Renderer) {
+value:string="";
+
+constructor() {
               this.names=["Nayan","Nuruzzaman","Bepari"];
    
   }
+getColor(el):void{
+   this.value =el.style.color; 
+   this.colorChange.emit(this.value);
+ }
+
 @Output() colorChange=new EventEmitter<string>(); 
 
- value:string="green";
-@HostListener("mouseover") onMouseOver(){
-   this.value = this.e.nativeElement.style.color;
-  
-}
+ 
 
 
 names:string[];
